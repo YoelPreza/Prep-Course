049ddb5e96da4969c0e40240c9034387d0c0932e
YoelPreza <preza_95@hotmail.com>
2022-09-27 21:58:36 -0500

    tarea html/css1

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -35,6 +35,11 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  var stringtoArray = str.split(' ')
+  var arrayFinal = stringtoArray.map(function(ele){
+    return ele.split('').reverse().join('');
+  })
+  return arrayFinal.join(' ')
 } 
 
 
